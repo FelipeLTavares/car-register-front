44224dee702dcd7a433dd200d2561946643c9b3c
Felipe Tavares <felipelimatavares96@gmail.com>
2022-11-04 23:20:49 -0300

    New interfaces created

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,12 +1,43 @@
 //Types
-type Cambio = "manual" | "automatico" | "automatizado" | "CVT" | "";
+export type Cambio = "manual" | "automatico" | "";
 
 //Interface
 export interface Carro {
+  placa: string;
   marca: string;
   modelo: string;
   cor: string;
   anoFabricacao: number;
   anoModelo: number;
-  tipoCambio: Cambio;
+  cambio: Cambio;
 }
+
+export interface CarroFilter {
+  placa: string | undefined;
+  marca: string | undefined;
+  modelo: string | undefined;
+  cor: string | undefined;
+  cambio: string | undefined;
+  anoModelo: {
+    de: number | undefined;
+    ate: number | undefined;
+  };
+  anoFabricacao: {
+    de: number | undefined;
+    ate: number | undefined;
+  };
+}
+
+//Props
+export interface CarInfoProps {
+  car: Carro;
+}
+
+export interface CarListProps {
+  list: Carro[];
+}
+
+/*   function teste() {
+    const API_URL = import.meta.env.VITE_API;
+    console.log(BASE_URL);
+  } */
