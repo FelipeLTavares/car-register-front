8664041fe9264a8ef84ba51aa0f4cd5d9cf2f2e7
Felipe Tavares <felipelimatavares96@gmail.com>
2022-11-04 23:16:32 -0300

    Add Router and some logic to App comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import RegisterForm from "./components/RegisterForm/RegisterForm"
-import { GlobalStyles } from "./styles/global.styles"
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CarList from "./components/CarList/CarList";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+import { Carro } from "./interfaces";
+import { GlobalStyles } from "./styles/global.styles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Views/Home";
 
 export default function App() {
-  GlobalStyles()
+  GlobalStyles();
+  const [carros, setCarros] = useState<Carro[] | undefined>();
+
+  useEffect(() => {
+    getCars();
+  }, []);
+
+  async function getCars() {
+    const carsList = await axios.get("http://localhost:8080/");
+    setCarros(carsList.data);
+  }
 
   return (
-    <div className="App">
-      <RegisterForm></RegisterForm>
-    </div>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<RegisterForm />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
