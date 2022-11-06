81e82a554f3c302c8f152ec6d4d8d2c7643abd19
Felipe Tavares <felipelimatavares96@gmail.com>
2022-11-06 17:20:22 -0300

    Context added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import CarList from "./components/CarList/CarList";
-import RegisterForm from "./components/RegisterForm/RegisterForm";
+import RegisterForm from "./components/Register/RegisterForm/RegisterForm";
 import { Carro } from "./interfaces";
 import { GlobalStyles } from "./styles/global.styles";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Views/Home";
+import AuthProvider from "./context/CarContext";
 
 export default function App() {
   GlobalStyles();
@@ -22,12 +22,14 @@ export default function App() {
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<RegisterForm />} />
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterForm />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
     </>
   );
 }
