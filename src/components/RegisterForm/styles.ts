9fba7685eeeb45980c583a305bc19f86c70773fb
Felipe Tavares <felipelimatavares96@gmail.com>
2022-11-06 17:20:45 -0300

    change folder

--- src/components/RegisterForm/styles.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from "@stitches/react";
-
-export const Container = styled("div", {
-  width: 360,
-  minHeight: 500,
-  margin: "20px auto",
-  border: "2px dashed blue",
-  borderRadius: 14,
-});
