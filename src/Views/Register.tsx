6ecc88e32efed7253a24d8f71c6916f5f3c38bc2
Felipe Tavares <felipelimatavares96@gmail.com>
2022-11-06 17:23:29 -0300

    folder change

--- src/Views/Register.tsx
+++ src/Views/Register.tsx
@@ -1,4 +1,4 @@
-import RegisterForm from "../components/RegisterForm/RegisterForm";
+import RegisterForm from "../components/Register/RegisterForm/RegisterForm";
 
 export default function Register() {
   return <RegisterForm />;
